6724e3f621523c95f2d6785b8b14992626d442a0
Thijs Suijten <tsuijten@gmail.com>
2015-08-07 11:10:45 +0200

    Added sample app

---
+++ sampleapp/src/main/java/scrollingimageview/android/q42/com/sampleapp/MainActivity.java
@@ -0,0 +1,12 @@
+package scrollingimageview.android.q42.com.sampleapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
